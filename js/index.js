0e80bb1c8f146d615da2d9dc78d5e7276253d993
Victor Rose <victordrose@gmail.com>
2017-08-02 16:36:30 -0400

    File Sturcture
    Where I am right now

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//http://transparencyinteractive.com/about/img/header.gif
+//https://media.giphy.com/media/aQ8hC3sok8khi/giphy.gif
+
+function Enter(){
+  document.getElementById("enter").style.display="none";
+  document.getElementById("navmenu").style.display="inline";
+  document.getElementById("homebtn").style.display="block";
+  document.getElementById("Title").style.display="none";
+}
+function home(){
+  document.getElementById("enter").style.display="block";
+  document.getElementById("navmenu").style.display="none";
+  document.getElementById("homebtn").style.display="none";
+  document.getElementById("Title").style.display="block"; 
+}
